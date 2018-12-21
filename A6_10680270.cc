9d246886b9fc44dd96b7a2555b2a11a83c2bce36
kingsosa06 <42818265+kingsosa06@users.noreply.github.com>
2018-12-21 08:58:00 +0000

    Add files via upload

---
+++ A6_10680270.cc
@@ -0,0 +1,32 @@
+// Name: Fuseini Iddrisu Tahiru
+// ID: 10680270
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int numb[7];
+    int i, j;
+
+    for(i=0;i<=6;i++){
+        cout << "Please enter number: ";
+        cin >> numb[i];
+        }
+    for(i=0;i<=5;i++){
+        for(j=i+1;j<=6;j++){
+            int temp;
+
+            if(numb[i] > numb[j]){
+                temp = numb[i];
+                numb[i] = numb[j];
+                numb[j] = temp;
+                }
+            }
+        }
+
+        for(i=0;i<=6;i++){
+        cout << endl << numb[i] << endl;
+        }
+return 0;
+}
+
